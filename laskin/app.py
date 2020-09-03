e64e351d8fe0a70318154fc0e98b198a87a5ecbf
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-03 11:53:02 +1000

    Add custom server error

--- laskin/app.py
+++ laskin/app.py
@@ -1,5 +1,14 @@
+from http import HTTPStatus as status
+
 import connexion
+
+
+def handle_server_error(error):
+    return "", status.INTERNAL_SERVER_ERROR
+
 
 app = connexion.App(__name__, specification_dir="./specs")
 app.add_api("laskin.yml")
-app.run()
+app.add_error_handler(status.INTERNAL_SERVER_ERROR, handle_server_error)
+
+app.run(debug=False)
