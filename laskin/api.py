2bae264367f36ff4afed66259c87d0fdddeb2d42
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-03 11:16:16 +1000

    Implement other comparison operations

--- laskin/api.py
+++ laskin/api.py
@@ -5,5 +5,21 @@ def hello():
     return {"message": "Hello!"}, 200
 
 
+def less(body):
+    return comparison_operation(data=body, operation=operator.lt)
+
+
+def greater(body):
+    return comparison_operation(data=body, operation=operator.gt)
+
+
+def less_or_equal(body):
+    return comparison_operation(data=body, operation=operator.le)
+
+
 def greater_or_equal(body):
     return comparison_operation(data=body, operation=operator.ge)
+
+
+def equal(body):
+    return comparison_operation(data=body, operation=operator.eq)
