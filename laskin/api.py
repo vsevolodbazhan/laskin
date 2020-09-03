16ea39472c104f29339fec27a2e8e0f40a7994ef
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-09-03 10:54:58 +1000

    Add `greater-or-equal` operation

--- laskin/api.py
+++ laskin/api.py
@@ -1,2 +1,18 @@
+from connexion import NoContent
+
+
 def hello():
     return {"message": "Hello!"}, 200
+
+
+def extract_numbers(payload):
+    a, b = payload["a"], payload["b"]
+    return float(a), float(b)
+
+
+def greater_or_equal(body):
+    a, b = extract_numbers(body)
+
+    if a >= b:
+        return NoContent, 200
+    return NoContent, 204
